2c830d96c7c70b1599fa06e312e90e506e051679
Kenny Chua <kennychua13@gmail.com>
2022-10-25 11:25:22 +0800

    Added stationary rectangle function
    A function for drawing stationary rectangles of varying sizes.

--- Src/level.c
+++ Src/level.c
@@ -13,6 +13,11 @@ void Level_Update()
 	CP_Graphics_ClearBackground(CP_Color_Create(135, 206, 250, 255));
 
 	CP_Graphics_DrawRect(CP_Input_GetMouseX(), CP_Input_GetMouseY(), 25.0f, 25.0f);
+	
+	// Kenny
+	// Set starting point of rectangles
+	CP_Settings_RectMode(CP_POSITION_CORNER);
+	CP_Graphics_DrawRectAdvanced(CP_System_GetWindowWidth() / 12.8f, CP_System_GetWindowHeight() / 7.2f, 50.f, 20.f, 90.f, 0.f);
 
 	if (CP_Input_KeyTriggered(KEY_ESCAPE)) {
 		CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);
