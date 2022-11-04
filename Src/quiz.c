cc1f8c2e9ec5f67b02f402ca9e6dfd7f405d8846
Wei Fong <tanwf20@gmail.com>
2022-11-04 15:29:22 +0800

    Addition of button to go game level

--- Src/quiz.c
+++ Src/quiz.c
@@ -6,13 +6,20 @@
 #include <mainmenu.h>
 #include "utils.h"
 #include <stdbool.h>
+#include<level.h>
 
 char TrueFalse_x1[4] = { 1 };
 char TrueFalse_y1[10] = { 2 };
 char TrueFalseans1[10] = { 3 };
+
 char TrueFalse_x2[4] = { 1 };
 char TrueFalse_y2[10] = { 2 };
 char TrueFalseans2[10] = { 3 };
+
+
+char TrueFalse_x3[4] = { 1 };
+char TrueFalse_y3[10] = { 2 };
+char TrueFalseans3[10] = { 3 };
 
 
 
@@ -33,15 +40,28 @@ void Quiz_Init()
 	sprintf_s(TrueFalse_x2, _countof(TrueFalse_x2), "%d", random_x2);
 	int random_y2 = CP_Random_RangeInt(0, 10);
 	sprintf_s(TrueFalse_y2, _countof(TrueFalse_y2), "%d", random_y2);
-	sprintf_s(TrueFalseans2, _countof(TrueFalseans2), "%d", random_y2 + random_x2);
+	sprintf_s(TrueFalseans2, _countof(TrueFalseans2), "%d",  random_y2*random_x2 + 3);
 
+	int random_x3 = CP_Random_RangeInt(0, 5);
+	sprintf_s(TrueFalse_x3, _countof(TrueFalse_x3), "%d", random_x3);
+	int random_y3 = CP_Random_RangeInt(0, 5);
+	sprintf_s(TrueFalse_y3, _countof(TrueFalse_y3), "%d", random_y3);
+	sprintf_s(TrueFalseans3, _countof(TrueFalseans3), "%d", random_y3 * random_x3 + 3);
 	
 }
 	
-	bool clicked = false;
-	bool question_1 = true;
+	bool clicked_q1	   = false;
 	bool question_1ans = false;
-	bool question_2 = false;
+	bool question_1	   = true;
+
+	bool question_2	   = false;
+	bool clicked_q2    = false;
+	bool question_2ans = false;
+
+	bool question_3	   = false;
+	bool clicked_q3 = false;
+	bool question_3ans = false;
+
 void Quiz_Update()
 {
 	int questionnum = 1;
@@ -76,7 +96,7 @@ void Quiz_Update()
 		{
 			if (IsAreaClicked(width / 2.4f, height / 1.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
 			{
-				 clicked = TRUE;
+				clicked_q1 = TRUE;
 			}
 			else if (IsAreaClicked(width / 1.7f, height / 1.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
 			{
@@ -85,17 +105,17 @@ void Quiz_Update()
 		
 
 		}
-			if (clicked == TRUE)
+			if (clicked_q1 == TRUE)
 			{
 				CP_Font_DrawText(" Correct ", width / 2, height / 1.8);
 				CP_Settings_Fill(CP_Color_Create(255, 255, 255, 0));
-				CP_Graphics_DrawRectAdvanced(width / 2, height / 2.4, 200, 100, 0, 5); // false button
+				CP_Graphics_DrawRectAdvanced(width / 1.3, height / 1.3, 200, 100, 0, 5); // false button
 				CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
-				CP_Font_DrawText(" Next ", width / 2, height / 2.4);
+				CP_Font_DrawText(" Next ", width / 1.3, height / 1.3);
 				//CP_Graphics_ClearBackground(CP_Color_Create(135, 206, 250, 255));
 				if (CP_Input_MouseDown(MOUSE_BUTTON_LEFT))
 				{
-					if (IsAreaClicked(width /2.0f, height / 2.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+					if (IsAreaClicked(width /1.3f, height / 1.3f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
 					{
 						CP_Graphics_ClearBackground(CP_Color_Create(135, 206, 250, 255));
 						question_1 = FALSE;
@@ -103,7 +123,7 @@ void Quiz_Update()
 					}
 				}
 			}
-			if((question_1ans == TRUE) && (clicked == FALSE))
+			if((question_1ans == TRUE) && (clicked_q1 == FALSE))
 			{
 				CP_Font_DrawText(" Wrong ", width / 2, height / 1.8);
 			}
@@ -113,10 +133,11 @@ void Quiz_Update()
 	{
 
 		//Drawing and setting the start rectangle position
-		CP_Font_DrawText("If y =      and x =  ", width / 2, height / 2);
-		CP_Font_DrawText(TrueFalse_x2, width / 2.15, height / 2);
-		CP_Font_DrawText(TrueFalse_y2, width / 1.65, height / 2);
-		CP_Font_DrawText(" Does x and y equals to       ?", width / 2, height / 1.6);
+		CP_Font_DrawText("If x =      ", width / 2, height / 2);
+		CP_Font_DrawText(TrueFalse_x2, width / 1.9, height / 2);
+		CP_Font_DrawText(" Does      x + 3 equals to       ?", width / 2, height / 1.6);
+		CP_Font_DrawText(TrueFalse_y2, width / 2.4, height / 1.6);
+		//CP_Font_DrawText(TrueFalse_x2, width / 2.15, height / 1.6);
 		CP_Font_DrawText(TrueFalseans2, width / 1.55, height / 1.6);
 
 		CP_Settings_Fill(CP_Color_Create(255, 255, 255, 0));
@@ -132,22 +153,213 @@ void Quiz_Update()
 		{
 			if (IsAreaClicked(width / 2.4f, height / 1.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
 			{
-				CP_Font_DrawText(" Correct ", width / 2, height / 1.8);
+				clicked_q2 = TRUE;
 			}
 			else if (IsAreaClicked(width / 1.7f, height / 1.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
 			{
-				CP_Font_DrawText(" Wrong ", width / 2, height / 1.8);
+				question_2ans = TRUE;
 			}
+		}
+		if (clicked_q2 == TRUE)
+		{
+			CP_Font_DrawText(" Correct ", width / 2, height / 1.8);
+			CP_Settings_Fill(CP_Color_Create(255, 255, 255, 0));
+			CP_Graphics_DrawRectAdvanced(width / 1.3, height / 1.3, 200, 100, 0, 5); // false button
+			CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+			CP_Font_DrawText(" Next ", width / 1.3, height / 1.3);
+			//CP_Graphics_ClearBackground(CP_Color_Create(135, 206, 250, 255));
+			if (CP_Input_MouseDown(MOUSE_BUTTON_LEFT))
+			{
+				if (IsAreaClicked(width / 1.3f, height / 1.3f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+				{
+					//CP_Graphics_ClearBackground(CP_Color_Create(135, 206, 250, 255));
+					question_3 = TRUE;
+					question_2 = FALSE;
+				}
+			}
+		}
+		if ((clicked_q2 == FALSE) && (question_2ans == TRUE))
+		{
+			CP_Font_DrawText(" Wrong ", width / 2, height / 1.8);
+		}
+	}
+
+
+	if (question_2 == TRUE )
+	{
+
+		//Drawing and setting the start rectangle position
+		CP_Font_DrawText("If x =      ", width / 2, height / 2);
+		CP_Font_DrawText(TrueFalse_x2, width / 1.9, height / 2);
+		CP_Font_DrawText(" Does      x + 3 equals to       ?", width / 2, height / 1.6);
+		CP_Font_DrawText(TrueFalse_y2, width / 2.4, height / 1.6);
+		//CP_Font_DrawText(TrueFalse_x2, width / 2.15, height / 1.6);
+		CP_Font_DrawText(TrueFalseans2, width / 1.55, height / 1.6);
+
+		CP_Settings_Fill(CP_Color_Create(255, 255, 255, 0));
+		CP_Graphics_DrawRectAdvanced(width / 1.7, height / 1.4, 200, 100, 0, 5); // false button
+		CP_Graphics_DrawRectAdvanced(width / 2.4, height / 1.4, 200, 100, 0, 5); // true button
+
+		CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+		CP_Font_DrawText(" True ", width / 2.4, height / 1.4);
+		CP_Font_DrawText(" False ", width / 1.7, height / 1.4);
+
+		//If mouse click is within a certain area
+		if (CP_Input_MouseDown(MOUSE_BUTTON_LEFT))
+		{
+			if (IsAreaClicked(width / 2.4f, height / 1.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+			{
+				clicked_q2 = TRUE;
+			}
+			else if (IsAreaClicked(width / 1.7f, height / 1.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+			{
+				question_2ans = TRUE;
+			}
+		}
+		if (clicked_q2 == TRUE)
+		{
+			CP_Font_DrawText(" Correct ", width / 2, height / 1.8);
+			CP_Settings_Fill(CP_Color_Create(255, 255, 255, 0));
+			CP_Graphics_DrawRectAdvanced(width / 1.3, height / 1.3, 200, 100, 0, 5); // false button
+			CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+			CP_Font_DrawText(" Next ", width / 1.3, height / 1.3);
+			//CP_Graphics_ClearBackground(CP_Color_Create(135, 206, 250, 255));
+			if (CP_Input_MouseDown(MOUSE_BUTTON_LEFT))
+			{
+				if (IsAreaClicked(width / 1.3f, height / 1.3f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+				{
+					//CP_Graphics_ClearBackground(CP_Color_Create(135, 206, 250, 255));
+					question_3 = TRUE;
+					question_2 = FALSE;
+				}
+			}
+		}
+		if ((clicked_q2 == FALSE) && (question_2ans == TRUE))
+		{
+			CP_Font_DrawText(" Wrong ", width / 2, height / 1.8);
 		}
 	}
 	
+
+
+	if (question_2 == TRUE )
+	{
+
+		//Drawing and setting the start rectangle position
+		CP_Font_DrawText("If x =      ", width / 2, height / 2);
+		CP_Font_DrawText(TrueFalse_x2, width / 1.9, height / 2);
+		CP_Font_DrawText(" Does      x + 3 equals to       ?", width / 2, height / 1.6);
+		CP_Font_DrawText(TrueFalse_y2, width / 2.4, height / 1.6);
+		//CP_Font_DrawText(TrueFalse_x2, width / 2.15, height / 1.6);
+		CP_Font_DrawText(TrueFalseans2, width / 1.55, height / 1.6);
+
+		CP_Settings_Fill(CP_Color_Create(255, 255, 255, 0));
+		CP_Graphics_DrawRectAdvanced(width / 1.7, height / 1.4, 200, 100, 0, 5); // false button
+		CP_Graphics_DrawRectAdvanced(width / 2.4, height / 1.4, 200, 100, 0, 5); // true button
+
+		CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+		CP_Font_DrawText(" True ", width / 2.4, height / 1.4);
+		CP_Font_DrawText(" False ", width / 1.7, height / 1.4);
+
+		//If mouse click is within a certain area
+		if (CP_Input_MouseDown(MOUSE_BUTTON_LEFT))
+		{
+			if (IsAreaClicked(width / 2.4f, height / 1.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+			{
+				clicked_q2 = TRUE;
+			}
+			else if (IsAreaClicked(width / 1.7f, height / 1.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+			{
+				question_2ans = TRUE;
+			}
+		}
+		if (clicked_q2 == TRUE)
+		{
+			CP_Font_DrawText(" Correct ", width / 2, height / 1.8);
+			CP_Settings_Fill(CP_Color_Create(255, 255, 255, 0));
+			CP_Graphics_DrawRectAdvanced(width / 1.3, height / 1.3, 200, 100, 0, 5); // false button
+			CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+			CP_Font_DrawText(" Next ", width / 1.3, height / 1.3);
+			//CP_Graphics_ClearBackground(CP_Color_Create(135, 206, 250, 255));
+			if (CP_Input_MouseDown(MOUSE_BUTTON_LEFT))
+			{
+				if (IsAreaClicked(width / 1.3f, height / 1.3f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+				{
+					//CP_Graphics_ClearBackground(CP_Color_Create(135, 206, 250, 255));
+					question_3 = TRUE;
+					question_2 = FALSE;
+				}
+			}
+		}
+		if ((clicked_q2 == FALSE) && (question_2ans == TRUE))
+		{
+			CP_Font_DrawText(" Wrong ", width / 2, height / 1.8);
+		}
+	}
+
+
+
+	if (question_3 == TRUE)
+	{
+
+		//Drawing and setting the start rectangle position
+		CP_Font_DrawText("If x =      ", width / 2, height / 2);
+		CP_Font_DrawText(TrueFalse_x3, width / 1.9, height / 2);
+		CP_Font_DrawText(" Does      x + 3 equals to       ?", width / 2, height / 1.6);
+		CP_Font_DrawText(TrueFalse_y3, width / 2.4, height / 1.6);
+		CP_Font_DrawText(TrueFalseans3, width / 1.55, height / 1.6);
+
+		CP_Settings_Fill(CP_Color_Create(255, 255, 255, 0));
+		CP_Graphics_DrawRectAdvanced(width / 1.7, height / 1.4, 200, 100, 0, 5); // false button
+		CP_Graphics_DrawRectAdvanced(width / 2.4, height / 1.4, 200, 100, 0, 5); // true button
+
+		CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+		CP_Font_DrawText(" True ", width / 2.4, height / 1.4);
+		CP_Font_DrawText(" False ", width / 1.7, height / 1.4);
+
+		//If mouse click is within a certain area
+		if (CP_Input_MouseDown(MOUSE_BUTTON_LEFT))
+		{
+			if (IsAreaClicked(width / 2.4f, height / 1.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+			{
+				clicked_q3 = TRUE;
+			}
+			else if (IsAreaClicked(width / 1.7f, height / 1.4f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+			{
+				question_3ans = TRUE;
+			}
+		}
+		if (clicked_q3 == TRUE)
+		{
+			CP_Font_DrawText(" Correct ", width / 2, height / 1.8);
+			CP_Settings_Fill(CP_Color_Create(255, 255, 255, 0));
+			CP_Graphics_DrawRectAdvanced(width / 1.3, height / 1.3, 200, 100, 0, 5); // false button
+			CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+			CP_Font_DrawText(" Continue game ", width / 1.3, height / 1.3);
+			//CP_Graphics_ClearBackground(CP_Color_Create(135, 206, 250, 255));
+			if (CP_Input_MouseDown(MOUSE_BUTTON_LEFT))
+			{
+				if (IsAreaClicked(width / 1.3f, height / 1.3f, 200.f, 100.f, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+				{
+					CP_Engine_SetNextGameState(Level_Init, Level_Update, Level_Exit);
+					
+				}
+			}
+		}
+		if ((clicked_q2 == FALSE) && (question_2ans == TRUE))
+		{
+			CP_Font_DrawText(" Wrong ", width / 2, height / 1.8);
+		}
+	}
+
+
 
 
 	// Pressing Q to exit the program
 	if (CP_Input_KeyTriggered(KEY_Q))
 	{
 		CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit); 
-	}
+	}			
 	
 		          
 }
