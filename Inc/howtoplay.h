d313e99c17bff4d1376ace9cdf6a5e448d32c402
Kenny Chua <kennychua13@gmail.com>
2022-11-27 16:33:06 +0800

    Updated howtoplay.h documentation

--- Inc/howtoplay.h
+++ Inc/howtoplay.h
@@ -1,3 +1,16 @@
+/*---------------------------------------------------------------
+Math Run by Team Rocket
+Class: CSD1401A Fall 2022
+File: howtoplay.h
+Authors: Chua Wen Bin Kenny, c.wenbinkenny@digipen.edu
+
+Description:
+Declaration of the initialization, update, and exit state of
+the how to play page.
+
+Copyright: 2022, Digipen Institute of Technology, Singapore
+----------------------------------------------------------------*/
+
 #pragma once
 
 void HowToPlay_Init(void);
